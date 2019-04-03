3e3b656ecccd73287ad4bb5557d2a2a13a7ec7e6
Lukas 'Arziel' Klima <arziel12@gmail.com>
2019-04-03 09:26:05 +0200

    allow symfony/console 3

--- src/FancyProgressBar.php
+++ src/FancyProgressBar.php
@@ -8,10 +8,8 @@ use Symfony\Component\Console\Helper\ProgressBar;
 class FancyProgressBar
 {
 	protected const DEFAULT_STEP = 1;
-	
 	protected const COLOR_YELLOW = 'yellow';
 	protected const COLOR_GREEN = 'green';
-	
 	/**
 	 * @var \Symfony\Component\Console\Helper\ProgressBar
 	 */
@@ -24,28 +22,47 @@ class FancyProgressBar
 	 * @var float
 	 */
 	private $timer;
+	/**
+	 * @var string
+	 */
+	private $name;
 	
-	public function __construct(\Symfony\Component\Console\Output\OutputInterface $output)
+	private $precision = 3;
+	
+	public function __construct(\Symfony\Component\Console\Output\OutputInterface $output, string $name)
 	{
 		$this->output = $output;
+		$this->name = $name;
 		
-		$output->getFormatter()->setStyle(self::COLOR_YELLOW, new OutputFormatterStyle(self::COLOR_YELLOW, 'default', []));
-		$output->getFormatter()->setStyle(self::COLOR_GREEN, new OutputFormatterStyle(self::COLOR_GREEN, 'default', ['bold']));
-		
+		$output->getFormatter()->setStyle(
+			self::COLOR_YELLOW,
+			new OutputFormatterStyle(self::COLOR_YELLOW, 'default', [])
+		);
+		$output->getFormatter()->setStyle(
+			self::COLOR_GREEN,
+			new OutputFormatterStyle(self::COLOR_GREEN, 'default', ['bold'])
+		);
 		
 		$this->progressBar = new ProgressBar($output);
 		
 		$this->progressBar->setMessage('', 'info');
-	
+		
 		$this->progressBar->setFormat($this->buildFormat());
-		$this->progressBar->setRedrawFrequency(10);
+		$this->progressBar->setRedrawFrequency(1);
+		
+		$this->start();
+	}
 	
-		$this->timer = 0 - \microtime(true);
+	public function setPrecision(int $precision): self
+	{
+		$this->precision = $precision;
+		
+		return $this;
 	}
 	
 	public function getEstimatedTime(): string
 	{
-		return \number_format(round($this->timer + \microtime(true), 3), 3);
+		return \number_format(round($this->timer + \microtime(true), $this->precision), $this->precision);
 	}
 	
 	public function advanceWithMessage(string $message, int $step = self::DEFAULT_STEP): void
@@ -84,13 +101,25 @@ class FancyProgressBar
 	
 	public function finish(): void
 	{
-		$this->progressBar->setFormat($this->buildFormat(self::COLOR_GREEN));
 		$this->progressBar->finish();
 		$this->progressBar->display();
+		
+		$this->output->writeln(
+			\sprintf(
+				\PHP_EOL . \PHP_EOL . ' <green>Task <yellow>\'%s\'</yellow> finished in %ss</green>' . PHP_EOL,
+				$this->name,
+				$this->getEstimatedTime()
+			)
+		);
 	}
 	
 	protected function buildFormat(string $timer = self::COLOR_YELLOW): string
 	{
 		return "<$timer>%timer:6s%s</$timer> %current%/%max% [%bar%] %percent:3s%% %info%";
+	}
+	
+	protected function start(): void
+	{
+		$this->timer = 0 - \microtime(true);
 	}
 }
